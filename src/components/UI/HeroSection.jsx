b62908edfef5de12a7508467642e14128bd0abea
aryanlimbachiya <alimbachiya960@gmail.com>
2025-08-28 10:45:19 +0530

    worldatlas

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,4 +1,5 @@
 import { FaLongArrowAltRight } from "react-icons/fa"
+import { NavLink } from "react-router-dom"
 
 export const HeroSection = () => {
     return (
@@ -7,7 +8,7 @@ export const HeroSection = () => {
                 <div className="hero-content">
                     <h1 className="heading-xl">Explore the World, One Country at a Time</h1>
                     <p className="paragraph">Discover the history, culture, and beauty of every nation. Sort, search, and filter through countries to find the details you need.</p>
-                    <button className="btn btn-darken btn-inline bg-white-box">Start Exploring <FaLongArrowAltRight/></button>
+                    <NavLink to="/country"><button className="btn btn-darken btn-inline bg-white-box">Start Exploring <FaLongArrowAltRight/></button></NavLink>
                 </div>
                 <div className="hero-image">
                     <img src="/images/world.png" alt="world is beauty" className="banner-image" />
